4db6a506689e330511630245276237077775cb87
Dan Amador <danamadorpe@gmail.com>
2019-03-23 18:51:13 +0100

    Helper function added to filter values that are not in model

--- Helpers/helper_functions.py
+++ Helpers/helper_functions.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 
 def get_or_create(session, dbModel, **kwargs):
     instance = session.query(dbModel).filter_by(**kwargs).first()
@@ -8,3 +10,13 @@ def get_or_create(session, dbModel, **kwargs):
         session.add(instance)
         session.commit()
         return instance
+
+
+# Filters incoming put requests so only the necessary fields are considered
+def filter_request(model, request: Dict) -> Dict:
+    to_return = {}
+    for k, v in request.items():
+        if k in model.__table__.columns.keys():
+            to_return[k] = v
+
+    return to_return
