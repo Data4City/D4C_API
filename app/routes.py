3c4b15f04a2ffb96f44f38a97a8479f09f34f9c8
Dan Amador <danamadorpe@gmail.com>
2019-04-19 16:29:51 +0200

    Forgot to add module

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 
-from Helpers.Middlewares import SQLAlchemySessionManager, Jsonify,
+from Helpers.Middlewares import SQLAlchemySessionManager, Jsonify, ResponseLoggerMiddleware
 from Resources import *
 
 engine = create_engine("sqlite:///sensor.db")#, echo=True)
