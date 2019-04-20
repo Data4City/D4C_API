47f5e975356318eacb9afa3665908505cca362ee
Dan Amador <danamadorpe@gmail.com>
2019-04-20 21:05:51 +0200

    Blergh

--- app/main.py
+++ app/main.py
@@ -6,3 +6,9 @@ logger.addHandler(logging.FileHandler('api.log'))
 logger.setLevel(logging.INFO)
 
 app = get_app()
+
+print("HOLA")
+
+from gunicorn.app.base import Application
+if __name__ == "__main__":
+    Application().run(app)
