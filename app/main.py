53b0d69e7a7bbe3992d6610899ea869c3375ff8e
Dan Amador <danamadorpe@gmail.com>
2019-05-14 12:26:42 +0200

    REfactored the routes out of the way

--- app/main.py
+++ app/main.py
@@ -1,8 +1,34 @@
-from routes import get_app
 import logging
+
+import falcon
+from falcon import API
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import create_database, database_exists
+
+import routes
+from Helpers.Middlewares import SQLAlchemySessionManager, Jsonify, ResponseLoggerMiddleware
+from Helpers.helper_functions import create_db_connection_url
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.FileHandler('api.log'))
 logger.setLevel(logging.INFO)
+
+
+def get_app() -> API:
+    engine = create_engine(create_db_connection_url(), echo=True)
+    if not database_exists(engine.url):
+        create_database(engine.url)
+    session_factory = sessionmaker(bind=engine)
+    Session = scoped_session(session_factory)
+
+    _app = falcon.API(middleware=[SQLAlchemySessionManager(Session), Jsonify.Middleware(help_messages=True),
+                                  ResponseLoggerMiddleware()])
+
+    routes.add_routes(_app)
+
+    return _app
+
 
 app = get_app()
--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,10 @@
-import falcon
-from falcon import API
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy_utils import create_database, database_exists
-
-from Helpers.Middlewares import SQLAlchemySessionManager, Jsonify, ResponseLoggerMiddleware
-from Helpers.helper_functions import create_db_connection_url
 from Resources import *
 
 
-def get_app() -> API:
-    engine = create_engine(create_db_connection_url(), echo=True)
-    if not database_exists(engine.url):
-        create_database(engine.url)
-    session_factory = sessionmaker(bind=engine)
-    Session = scoped_session(session_factory)
-
-    _app = falcon.API(middleware=[SQLAlchemySessionManager(Session), Jsonify.Middleware(help_messages=True),
-                                  ResponseLoggerMiddleware()])
+def add_routes(_app):
     _app.add_route('/v1/kit', KitResource())
     _app.add_route('/v1/{kit_id:int}/kit', GeneralKitResource())
     _app.add_route('/v1/{kit_id:int}/sensor', SensorResource())
     _app.add_route('/v1/measurement/{sensor_id:int}', MeasurementResource())
     _app.add_route('/v1/{kit_id:int}/{measurement_id:int}/values', ValueResource())
     _app.add_route('/debug', DebugResource())
-    return _app
