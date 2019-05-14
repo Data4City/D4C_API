ee52e2135bc18f41f89545998378ce028b29f28d
Dan Amador <danamadorpe@gmail.com>
2019-05-14 12:38:50 +0200

    Logging refactor

--- app/Helpers/Middlewares/response_logger_middleware.py
+++ app/Helpers/Middlewares/response_logger_middleware.py
@@ -1,10 +1,10 @@
-import falcon
 import logging
-
+import falcon
 
 class ResponseLoggerMiddleware:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
+        self.logger.info("Logging started")
 
     def process_request(self, req, resp):
-        self.logger.info('{0} {1} {2}'.format(req.method, req.relative_uri, resp.status[:3]))
+        self.logger.info('{0} {1} {2} {3}'.format(req.method, req.relative_uri, resp.status, req.query_string))
--- app/main.py
+++ app/main.py
@@ -11,20 +11,23 @@ import routes
 from Helpers.Middlewares import SQLAlchemySessionManager, Jsonify, ResponseLoggerMiddleware
 from Helpers.helper_functions import create_db_connection_url
 
-logger = logging.getLogger(__name__)
-logger.addHandler(logging.FileHandler('api.log'))
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("mainapp."+__name__)
+#logger.addHandler(logging.FileHandler('api.log'))
 logger.setLevel(logging.INFO)
 
 
 def get_app() -> API:
-    engine = create_engine(create_db_connection_url(), echo=True)
+    engine = create_engine(create_db_connection_url())
     if not database_exists(engine.url):
+        logger.info("Creating database")
         create_database(engine.url)
     session_factory = sessionmaker(bind=engine)
     Session = scoped_session(session_factory)
 
     _app = falcon.API(middleware=[SQLAlchemySessionManager(Session), Jsonify.Middleware(help_messages=True),
                                   ResponseLoggerMiddleware()])
+
 
     routes.add_routes(_app)
 
