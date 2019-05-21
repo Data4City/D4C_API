4b6be6687a1933ae6a68e6561de8f9dd2a874e67
Dan Amador <danamadorpe@gmail.com>
2019-05-21 19:58:15 +0200

    Added time measurement in request logger

--- app/Helpers/Middlewares/response_logger_middleware.py
+++ app/Helpers/Middlewares/response_logger_middleware.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import logging
 import falcon
 
@@ -7,5 +8,9 @@ class ResponseLoggerMiddleware:
         self.logger = logging.getLogger(__name__)
         self.logger.info("Logging started")
 
+    def process_response(self, req, resp, resource, req_succeeded):
+        delta = datetime.now() - req.context["start_time"]
+        self.logger.info('{0} {1} {2} {3} {4}μs'.format(req.method, req.relative_uri, resp.status, req.json, delta.microseconds))
+
     def process_request(self, req, resp):
-        self.logger.info('{0} {1} {2} {3}'.format(req.method, req.relative_uri, resp.status, req.json))
+        req.context["start_time"] = datetime.now()
