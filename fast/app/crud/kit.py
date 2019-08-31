1b01d808667510dadcd5f231c00c8a8ede33e78b
Dan Amador <danamadorpe@gmail.com>
2019-08-31 11:07:26 +0200

    Update and multiple query for kits done

--- fast/app/crud/kit.py
+++ fast/app/crud/kit.py
@@ -1,16 +1,16 @@
 from sqlalchemy.orm import Session
 from app.db_models.kit import Kit
-from typing import List, Optional
+from typing import List, Optional, Tuple
 from app.db.helpers import get_or_create
 
 
-def get_single(db_session: Session, *, kit_id: int):
-    return db_session.query(Kit).get(kit_id).first()
+def get_single(db_session: Session, *, kit_id: int) -> Kit:
+    return db_session.query(Kit).get(kit_id)
 
 
-def get_multi(db_session: Session, *, limit=10) -> List[Optional[Kit]]:
-    return db_session.query(Kit).limit(limit).all()
+def get_multi(db_session: Session, *, amount=10) -> List[Optional[Kit]]:
+    return db_session.query(Kit).limit(amount).all()
 
 
-def create_single(db_session: Session, *, serial: str):
+def create_single(db_session: Session, *, serial: str) -> Tuple[Kit, bool]:
     return get_or_create(db_session, Kit, serial=serial)
