ee7d4108f35b2ba8be9579e7df4cac8cc520238c
Dan Amador <danamadorpe@gmail.com>
2019-09-06 20:47:27 +0200

    Migrations for inferences and files added

--- app/db/base.py
+++ app/db/base.py
@@ -1,4 +1,6 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
 from app.db.base_class import Base  # noqa
-from app.db_models.kit import Kit
+from app.db_models.kit import Kit # noga
+from app.db_models.dbfile import DBFile # noga
+from app.db_models.inference import Inference, Labels  # noga
