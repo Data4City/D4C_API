aed5cd1d1816ac766bc3e9e400c73d8aa2dc45f8
Dan Amador <danamadorpe@gmail.com>
2019-09-10 16:01:24 +0200

    Web inference works

--- app/api/api_v1/endpoints/inference.py
+++ app/api/api_v1/endpoints/inference.py
@@ -1,47 +1,45 @@
+import librosa
+import tempfile
+import numpy as np
+import requests
+import json
+from os import path
 from fastapi import APIRouter, Depends, HTTPException, File, UploadFile
 from sqlalchemy.orm import Session
 from starlette.status import HTTP_400_BAD_REQUEST, HTTP_500_INTERNAL_SERVER_ERROR
 
 from app.api.utils.db import get_db
 from db_models.dbfile import DBFile
-from PIL import Image
-
 from db_models.inference import LabelsEnum
-import io
-import numpy
+
 router = APIRouter()
 
 
-async def get_inference(content):
-    img = Image.open(io.BytesIO(content))
-    interpreter = tf.lite.Interpreter(model_path="model.tflite")
-    interpreter.allocate_tensors()
+async def get_single_inference(content):
+    data = json.dumps({"signature_name": "serving_default", "instances": content.tolist()})
 
-    # Get input and output tensors.
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
+    json_response = requests.post("http://localhost:8501/v1/models/urbanSound/versions/1:predict", data=data,
+                                  headers={"content-type": "application/json"})
+    return json_response.json()['predictions']
 
-
-    floating_model = input_details[0]['dtype'] == np.float32
-    if floating_model:
-        input_data = (np.float32(input_data) - 127.5) / 127.5
-
-    input_shape = input_details[0]['shape']
-    interpreter.set_tensor(input_details[0]['index'], input_data)
-
-    interpreter.invoke()
-
-    output_data = interpreter.get_tensor(output_details[0]['index'])
-    results = np.squeeze(output_data)
-
-    return {LabelsEnum(i): float(val) for i, val in enumerate(results)}
 
 async def _process_image(content: bytes, db: Session, save_file: bool = True):
-    inference = await get_inference(content)
+    inference = await get_single_inference(content)
     if save_file:
         await _save_file(content, db)
 
     return inference
+
+
+async def get_features(filename):
+    y, sr = librosa.load(filename)
+    mfccs = np.mean(librosa.feature.mfcc(y, sr, n_mfcc=40).T, axis=0)
+    melspectrogram = np.mean(librosa.feature.melspectrogram(y=y, sr=sr, n_mels=40, fmax=8000).T, axis=0)
+    chroma_stft = np.mean(librosa.feature.chroma_stft(y=y, sr=sr, n_chroma=40).T, axis=0)
+    chroma_cq = np.mean(librosa.feature.chroma_cqt(y=y, sr=sr, n_chroma=40).T, axis=0)
+    chroma_cens = np.mean(librosa.feature.chroma_cens(y=y, sr=sr, n_chroma=40).T, axis=0)
+    return np.reshape(np.vstack((mfccs, melspectrogram, chroma_stft, chroma_cq, chroma_cens)), (1, 40, 5))
+
 
 async def _save_file(content, db: Session):
     newfile = DBFile(data=content)
@@ -53,14 +51,21 @@ async def _save_file(content, db: Session):
 async def upload_file(*, db: Session = Depends(get_db), file: UploadFile = File(...)):
     print(file.content_type)
     ctype = file.content_type
-
+    # if ctype == "image/jpeg":
+    #     await _process_image(await file.read(), db)
     try:
-        if ctype == "image/jpeg":
-            await _process_image(await file.read(), db)
-        # elif ctype is "audio/wav":
-        #     save_image(process_audio(await file.read()))
+        if ctype == "audio/wav":
+            with tempfile.NamedTemporaryFile(suffix=".wav", prefix=path.basename(__file__)) as tmp:
+                b = await file.read()
+                tmp.write(b)
+                return [{LabelsEnum(i).name: float("{0:.2f}".format(val)) for i, val in enumerate(result)} for result in
+                        await get_single_inference(await get_features(path.join(tempfile.gettempdir(), str(tmp.name))))]
+
+            # save_image(process_audio(await file.read()))
         else:
             raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="Only wav and jpeg supported")
+    except HTTPException as e:
+        raise e
     except Exception as e:
         print(e)
         raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to process file")
